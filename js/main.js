7dcb589a26984f6ffe3d01194d83c42b3e9e1bc1
BenjaminArroyo <benji_rikoflishas@hotmail.com>
2025-03-02 15:20:25 -0700

    se logro recursividad, Listo

--- js/main.js
+++ js/main.js
@@ -12,34 +12,43 @@ Este proyecto se enfoca únicamente en usar recursión para buscar un elemento e
 
  // Copy code
 // Lista de regalos
-const gifts = ["Muñeca", "Carro de juguete", "Rompecabezas", "Lego", "Pelota"];
+const gifts = ["Muñeca", "Carro de juguete", "Rompecabezas", "Lego", "Pelota", "Melodica", "Piano de Juguete","Bicicleta", 
+    "Patin del Diablo", "Patines", "Juego de Mesa", "Pistola de Juguete", "Figura de Accion", "Padelbol"];
 
 // TODO: Completa esta función para que busque recursivamente el regalo en la lista
-function findGift(gifts, giftName, index = 0) {
-    // Caso base: Si llegamos al final de la lista
-    if (index === gifts.length) {
-        index += 1;
-        return `${giftName} no está en la lista.`;
-    }
+function findGift(gifts, giftName, index = 0) 
+{
+        if(gifts[index] === giftName){
+            console.log(`El regalo, ${giftName}, se encuentra en esta posición ${index + 1}`);
+        }
+        
+        // Caso base: Si llegamos al final de la lista
+        if (gifts[index] != giftName) {
+            if(index > gifts.length){
+                console.log(`${giftName} no está en la lista.`);
+                return -1;
+            }else{
+                index += 1;
+                findGift(gifts, giftName, index);//De aqui se vuelve a llamar, haciendo uso de la RECURSIVIDAD
+            }
+        }
   // TODO: Incluye el caso base donde se llega al final de la lista.
   // TODO: Incluye el caso base donde se encuentra el regalo.
   // TODO: Realiza la llamada recursiva para seguir buscando el regalo.
 }
+
 // Casos de ejemplo:
 // Llama a la función con los datos de entrada y muestra el resultado en la consola.
 // Llama a la función y prueba con diferentes regalos
-let giftToFind = "Lego";
-console.log(findGift(gifts, giftToFind));
+let giftToFind = "Camión";
+findGift(gifts, giftToFind)
 
-if(gifts[index] === giftName){
-    console.log(`El regalo esta en esta posición`);
-    return findGift(gifts, giftName, index + 1);
-}
 
+//--------------------------------------------------------------------------
+// Caso cuando el regalo no está en la lista
+//giftToFind = "Camión";
+// Salida esperada:
+// "Camión no está en la lista."
 // Salida esperada:
 // "Lego está en la posición 3."
 
-// Caso cuando el regalo no está en la lista
-giftToFind = "Camión";
-// Salida esperada:
-// "Camión no está en la lista."
